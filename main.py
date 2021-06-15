29be5337ea4b1b3b3022fc1ca949daf41d39c19c
Chun-Hung Lin <r06942072@ntu.edu.tw>
2021-06-15 23:15:01 +0800

    calculator.py works

--- main.py
+++ main.py
@@ -1,17 +1,30 @@
 #!/usr/bin/python
-# -*- coding: utf-8 -*-
 from tkinter import *
 
-if __name__ == '__main__': #中文 
+def button_add():
+    return
+
+def button_click(number):
+    return
+
+def button_clear():
+    return
+
+if __name__ == '__main__': 
     print("Start script")
     root = Tk()
-    root.title("My calculator") #設定這個GUI的標題
+    root.title("My calculator")
     
-    e = Entry(root, width=35, borderwidth=5)
-    #e.grid(row=0, column=0, columnspan=30, padx=10, pady=10)
+    e = Entry(root, width=50)  #An input box
 
-    e.insert(0, "Enter your name: ")
-
+    # Define Buttons
+    # The size of the buttons are 20*20
+    button_1 = Button(root, text="1", padx=20, pady=20, command=button_add)
+    button_2 = Button(root, text="2", padx=20, pady=20, command=button_add)
+    
+    # Put the buttons on the screen
+    button_1.grid(row=3, column=0)
+    button_2.grid(row=3, column=2)
 
     root.mainloop()
     print("Finish script")
